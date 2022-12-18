c775a2dcba122daf513b13e289dbd1ec4560d8aa
ShekarBakkashetty <shekar9989.ww@gmail.com>
2022-12-18 18:58:00 +1100

    Assingment Added

--- Assingment 5.py
+++ Assingment 5.py
@@ -1,6 +1,40 @@
 import pandas as pd
-#import pyspark
-#from pyspark.sql import SparkSession
+import findspark
+findspark.init()
+import pyspark
+from pyspark.sql import SparkSession
 
-df = pd.read_excel('I:\C driver\Ingrity\Session 5 Python Assignment (1).xlsx')
-print(df.head())
+from pyspark import SparkContext
+from pyspark.sql import SQLContext
+#sc = SparkContext("local", "App Name")
+#sql = SQLContext(sc)
+
+def der_artb(df):
+    list = df['BRAND_ID'].unique()
+    a = ''
+    b = 0
+    for i in range(0, len(df)):
+        j = list[b]
+        if j == df.loc[i, 'BRAND_ID']:
+            if a == '':
+                a = df.loc[i, 'PARENT_CATEGORY_ID']
+                df.loc[i, 'Web_TREE'] = a
+            else:
+                a = a + '_' + df.loc[i, 'PARENT_CATEGORY_ID']
+                df.loc[i, 'Web_TREE'] = a
+        else:
+            a = ''
+            b = b + 1
+            if a == '':
+                a = df.loc[i, 'PARENT_CATEGORY_ID']
+                df.loc[i, 'Web_TREE'] = a
+
+if __name__ == "__main__":
+    df = pd.read_excel('I:\C driver\Ingrity\Session 5 Python Assignment (1).xlsx')
+    #spark = SparkSession.Builder().appName("Practice").master("local[2]").getOrCreate()
+    #df1 = pyspark.pandas.read_excel('I:\C driver\Ingrity\Session 5 Python Assignment (1).xlsx', header=TRUE)
+    der_artb(df)
+    print(df)
+
+    #df2 = sql.createDataFrame(df)
+
